3ab27c3f23e7ed5e1d428b4912a89d5829762f1c
esaric <34062977+esaric6@users.noreply.github.com>
2021-05-21 16:26:52 +0200

    Update index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,6 +6,9 @@ import styled from "styled-components";
 import {CalGrid} from "../CalGrid";
 import {CalNav} from "../CalNav";
 import {TitleBar} from "../TitleBar";
+import {Form} from "../Form";
+
+import * as EventService from "../../services/event.service";
 
 const ShadoWrap = styled('div')`
   border-bottom: 2px solid #464648;
@@ -20,23 +23,52 @@ const ShadoWrap = styled('div')`
 function App() {
   moment.locale('hr');
   
+  // Show and hide edit form
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  // Current event that will be shown in the form
+  const [eventItem, setEventItem] = useState(null);
+
+  // Keep track of month and current day
   const [today, setToday] = useState(moment());
   const startDay = today.clone().startOf("month").startOf("week");
   
+  // Navigate calendar months
   const prevMontHandler = () => setToday(prev => prev.clone().subtract(1, 'month'));
   const currMontHandler = () => setToday(moment());
   const nextMontHandler = () => setToday(prev => prev.clone().add(1, 'month'));
   
+  // Save reference of event for editing form and show form
+  const handleFormOpen = (eventItem) => {
+    if (isFormOpen) { return; }
+    setEventItem(eventItem);
+    setIsFormOpen(true);
+  };
+
+  // Clear event from editing form and hide form
+  const handleFormClose = () => {
+    setEventItem(null);
+    setIsFormOpen(false);
+  };
+
   return (
     <ShadoWrap>
-      <TitleBar />
+      <TitleBar newEventHandler={() => { handleFormOpen(null); }} />
       <CalNav
         today={today}
         prevMontHandler={prevMontHandler}
         currMontHandler={currMontHandler}
         nextMontHandler={nextMontHandler}
       />
-      <CalGrid startDay={startDay} today={today} />
+      <CalGrid startDay={startDay} today={today} editEventHandler={handleFormOpen} />
+      {isFormOpen ? (
+        <Form
+          eventItem={eventItem}
+          saveButtonHandler={EventService.saveEvent}
+          deleteButtonHandler={EventService.deleteEvent}
+          quitButtonHandler={handleFormClose}
+        />
+      ) : null}
     </ShadoWrap>
   );
 };
