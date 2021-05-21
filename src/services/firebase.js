c986be5e5baef0d1f5823d44f9d10cf1e44c4ea8
esaric <34062977+esaric6@users.noreply.github.com>
2021-05-21 16:27:49 +0200

    Create firebase.js

---
+++ src/services/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.firestore();
+database.enablePersistence();
+
+export default database;
