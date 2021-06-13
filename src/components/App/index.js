686033f9b5a73a52249b393a6d40e6390aeeeb34
esaric <34062977+esaric6@users.noreply.github.com>
2021-06-13 21:18:58 +0200

    Update index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -20,13 +20,17 @@ const ShadoWrap = styled('div')`
   overflow: hidden;
 `;
 
+//  Komponenta ima varijable stanja "isFormOpen" i "eventItem", te funkcije azuriranja
+
 function App() {
   moment.locale('hr');
   
-  // Show and hide edit form
+  //  Show and hide edit form
+  //  Varijabla "setIsFormOpen" je isprva postavljena na "false" i nije vidljiva
   const [isFormOpen, setIsFormOpen] = useState(false);
 
-  // Current event that will be shown in the form
+  //  Current event that will be shown in the form
+  //  Varijabla "setEventItem" je isprva postavljena na "null" otkako se ne ureduje nijedan dogadaj u formi
   const [eventItem, setEventItem] = useState(null);
 
   // Keep track of month and current day
@@ -50,6 +54,19 @@ function App() {
     setEventItem(null);
     setIsFormOpen(false);
   };
+
+  //  Klikom na gumb za stvaranje novog dogadaja, poziva se funkcija "handleFormOpen" sa parametrom "null" otkako se ne ureduje vec postojeci dogadaj, vec se kreira potpuno novi
+  //  Komponenti "CalGrid"se predaje funkcija "editEventHandler" koja poziva funkciju "handleFormOpen"
+  //  Funkcija "handleFormOpen" sprjecava ponovno otvaranje iste forme; te postavlja parametar u varijabli "eventItem", a varijablu "isFormOpen" u "true"
+  //  Funkcija "handleFormClose" postavlja varijablu "eventItem" u "null" - i "isFormOpen" u "false" - kako bi se forma zatvorila, odnosno uklonila
+  //  Forma se prikazuje ako je varijable "isFormOpen" "true", cime se renderira komponenta "Form"; inace se ne prikazuje ništa
+  /*
+    Komponenti "Form" se predaju parametri:
+      - "eventItem" --> iz varijable stanja (prethodno postavljena funkcijom "setEventItem")
+      - "saveButtonHandler" --> funkcija iz "event.service.js"
+      - "deleteButtonHandler" --> funkcija iz "event.service.js"
+      - "quitButtonHandler" - funkcija iz L86
+  */
 
   return (
     <ShadoWrap>
