122e782fcb286f2adbcd0c4492fc45f81b9209d7
esaric <34062977+esaric6@users.noreply.github.com>
2021-06-14 10:51:26 +0200

    Update index.js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -21,54 +21,46 @@ const ShadoWrap = styled('div')`
 `;
 
 //  Komponenta ima varijable stanja "isFormOpen" i "eventItem", te funkcije azuriranja
-
 function App() {
   moment.locale('hr');
   
-  //  Show and hide edit form
   //  Varijabla "setIsFormOpen" je isprva postavljena na "false" i nije vidljiva
   const [isFormOpen, setIsFormOpen] = useState(false);
 
-  //  Current event that will be shown in the form
   //  Varijabla "setEventItem" je isprva postavljena na "null" otkako se ne ureduje nijedan dogadaj u formi
   const [eventItem, setEventItem] = useState(null);
 
-  // Keep track of month and current day
   const [today, setToday] = useState(moment());
   const startDay = today.clone().startOf("month").startOf("week");
   
-  // Navigate calendar months
   const prevMontHandler = () => setToday(prev => prev.clone().subtract(1, 'month'));
   const currMontHandler = () => setToday(moment());
   const nextMontHandler = () => setToday(prev => prev.clone().add(1, 'month'));
   
-  // Save reference of event for editing form and show form
   const handleFormOpen = (eventItem) => {
     if (isFormOpen) { return; }
     setEventItem(eventItem);
     setIsFormOpen(true);
   };
 
-  // Clear event from editing form and hide form
   const handleFormClose = () => {
     setEventItem(null);
     setIsFormOpen(false);
   };
 
-  //  Klikom na gumb za stvaranje novog dogadaja, poziva se funkcija "handleFormOpen" sa parametrom "null" otkako se ne ureduje vec postojeci dogadaj, vec se kreira potpuno novi
-  //  Komponenti "CalGrid"se predaje funkcija "editEventHandler" koja poziva funkciju "handleFormOpen"
-  //  Funkcija "handleFormOpen" sprjecava ponovno otvaranje iste forme; te postavlja parametar u varijabli "eventItem", a varijablu "isFormOpen" u "true"
-  //  Funkcija "handleFormClose" postavlja varijablu "eventItem" u "null" - i "isFormOpen" u "false" - kako bi se forma zatvorila, odnosno uklonila
-  //  Forma se prikazuje ako je varijable "isFormOpen" "true", cime se renderira komponenta "Form"; inace se ne prikazuje ništa
-  /*
-    Komponenti "Form" se predaju parametri:
-      - "eventItem" --> iz varijable stanja (prethodno postavljena funkcijom "setEventItem")
-      - "saveButtonHandler" --> funkcija iz "event.service.js"
-      - "deleteButtonHandler" --> funkcija iz "event.service.js"
-      - "quitButtonHandler" - funkcija iz L86
-  */
-
-  return (
+    return (
+    //  Klikom na gumb za stvaranje novog dogadaja, poziva se funkcija "handleFormOpen" sa parametrom "null" otkako se ne ureduje vec postojeci dogadaj, vec se kreira potpuno novi
+    //  Komponenti "CalGrid"se predaje funkcija "editEventHandler" koja poziva funkciju "handleFormOpen"
+    //  Funkcija "handleFormOpen" sprjecava ponovno otvaranje iste forme; te postavlja parametar u varijabli "eventItem", a varijablu "isFormOpen" u "true"
+      //  "isFormOpen" je uvijet: ako je istinit, komponenta "Form" (forma) se prikazuje uz navedene parametre, inace se ne prikazuje (ako je "null")
+    //  Funkcija "handleFormClose" postavlja varijablu "eventItem" u "null" - i "isFormOpen" u "false" - kako bi se forma zatvorila, odnosno uklonila
+    /*
+      Komponenti "Form" se predaju parametri:
+        - "eventItem" --> iz varijable stanja (prethodno postavljena funkcijom "setEventItem")
+        - "saveButtonHandler" --> funkcija "saveEvent" iz "event.service.js"
+        - "deleteButtonHandler" --> funkcija "deleteEvent" iz "event.service.js"
+        - "quitButtonHandler"
+    */
     <ShadoWrap>
       <TitleBar newEventHandler={() => { handleFormOpen(null); }} />
       <CalNav
